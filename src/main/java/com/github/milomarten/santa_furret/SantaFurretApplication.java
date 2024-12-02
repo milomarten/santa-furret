0d686b137405c8904fc88e7bb854a71198cd98a2
milomarten <milomartenstreams@gmail.com>
2024-12-01 18:00:15 -0600

    Initial commit

---
+++ src/main/java/com/github/milomarten/santa_furret/SantaFurretApplication.java
@@ -0,0 +1,13 @@
+package com.github.milomarten.santa_furret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaFurretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaFurretApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/milomarten/santa_furret/SantaFurretApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.milomarten.santa_furret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaFurretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
